b7fcccf5f16b7cb0b5a248844a45c866b4b78da1
Miguel Reyes <miguelreyher.marh@gmail.com>
2021-10-23 22:03:52 -0500

    Añadí explicación de \t y \n
    clase 21.10.2021

--- HolaMundo.cpp
+++ HolaMundo.cpp
@@ -1,29 +1,29 @@
 /*
 Programa que saluda al usuario
-Elaboró: MARH
+ElaborÃ³: MARH
 Fecha: 15/10/2021 
 */
 
-#include <stdio.h> //Archivo de cabecera que incluye entradas y salidas estándar 
+#include <stdio.h> //Archivo de cabecera que incluye entradas y salidas estÃ¡ndar 
 #include <conio.h> //Archivo que da control de las entradas y salidas
-#include <string.h> //Esta librería se usa por si no funciona el "gets"
+#include <string.h> //Esta librerÃ­a se usa por si no funciona el "gets"
 
 /*
 Este es 
 un comentario de
-párrafo
+pÃ¡rrafo
 */
 
-//Este es un comentario de línea
+//Este es un comentario de lÃ­nea
 
 main () {
 	printf ("Hola mundo. (Pesione la tecla ENTER)");
 	getch (); //DAR CARACTER, NOS DA TIEMPO DE VISUALIZACION EN EL ARCHIVO .EXE
 	
-	char TUNOMBRE [10]; //DECLARA VARIABLE DE TIPO CARACTER {TAMAÑO DE LA VARIABLE EN BITES]
-	printf ("Escribe tu nombre: ");
-	//scanf ("%s", TUNOMBRE); //LEE EL TECLADO {FORMATO}, %s = string. SE CAMBIÓ POR GETS PARA QUE LEA LOS ESPACIOS EN LOS NOMBRES.
+	char TUNOMBRE [10]; //DECLARA VARIABLE DE TIPO CARACTER {TAMAÃ‘O DE LA VARIABLE EN BITES]
+	printf ("\nEscribe tu nombre: "); //EL "\n" PERMITE UN SALTO ENTRE LOS PRINTF EN LA TERMINAL
+	//scanf ("%s", TUNOMBRE); //LEE EL TECLADO {FORMATO}, %s = string. SE CAMBIÃ“ POR GETS PARA QUE LEA LOS ESPACIOS EN LOS NOMBRES.
 	gets (TUNOMBRE); //PERMITE CONTATENAR CADENAS DE CARACTERES CON ESPACIOS
-	printf ("Hola %s", TUNOMBRE);
+	printf ("Hola \t%s", TUNOMBRE); //EL "\t" PERMITE UN ESPACIO DE TABULACIÃ“N DENTRO DE LA TERMINAl
 	getch ();
 }
